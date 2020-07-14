b4c06cb396f2c898a1ef88c896828204d3d20735
hafizsheetab <muhtaseen@iut-dhaka.edu>
2020-07-14 15:10:27 +0600

    A little modification to the previous code to make functions do only and only one thing

--- solutions/hafizsheetab/IssueNo8/src/ShapeList.java
+++ solutions/hafizsheetab/IssueNo8/src/ShapeList.java
@@ -2,40 +2,45 @@ public class ShapeList {
    private double[] sizes;
    private String[] types;
    private  int numberOfShapes;
+   private Shape [] shapes;
 
     public ShapeList(double[] sizes, String[] types) {
        this.sizes = sizes;
        this.types = types;
        this.numberOfShapes = sizes.length;
+       this.shapes=setShapes(sizes,types);
 
     }
+
+    private Shape[] setShapes(double[] sizes, String[] types) {
+        Shape[] shape = new Shape[numberOfShapes];
+        for(int i=0;i<numberOfShapes;i++){
+            shape[i] = getShapeType(types[i]);
+            shape[i].setSize(sizes[i]);
+        }
+        return shape;
+    }
+
     public double totalArea() {
         double total = 0;
-        Shape shape;
         for (int i = 0; i < numberOfShapes; i++) {
-            shape = getShape(types[i]);
-            shape.setSize(sizes[i]);
-            total += shape.area();
+            total += shapes[i].area();
         }
-
         return total;
     }
 
     public double totalPerimeter() {
 
         double total = 0;
-        Shape shape;
         for (int i = 0; i < numberOfShapes; i++) {
-            shape = getShape(types[i]);
-            shape.setSize(sizes[i]);
-            total += shape.perimeter();
+            total += shapes[i].perimeter();
         }
 
         return total;
     }
 
-    private static Shape getShape(String type){
-        if(type=="square"){
+    private static Shape getShapeType(String type){
+        if(type.equals("square")){
             return new Square();
         }
         else{
