d9ad37f3481fb84b0d833fb02c948f7690bdbcfd
hafizsheetab <muhtaseen@iut-dhaka.edu>
2020-07-14 16:34:40 +0600

    Solution #8 (revised)

--- solutions/hafizsheetab/IssueNo8/src/ShapeList.java
+++ solutions/hafizsheetab/IssueNo8/src/ShapeList.java
@@ -1,39 +1,17 @@
 public class ShapeList {
-   private double[] size;
-   private String[] type;
    private  int numberOfShapes;
-   private Shape [] shape;
+   private Shape [] shapes;
 
-    public ShapeList(double[] size, String[] type) {
-       this.size = size;
-       this.type = type;
-       this.numberOfShapes = size.length;
-       this.shape =setShapes();
+    public ShapeList(Shape [] shape) {
+       this.numberOfShapes = shape.length;
+       this.shapes =shape;
 
-    }
-
-    private Shape[] setShapes() {
-        Shape[] shape = new Shape[numberOfShapes];
-        for(int i=0;i<numberOfShapes;i++){
-            shape[i] = getShapeType(type[i]);
-            shape[i].setSize(size[i]);
-        }
-        return shape;
-    }
-
-    private static Shape getShapeType(String type){
-        if(type.equals("square")){
-            return new Square();
-        }
-        else{
-            return new Circle();
-        }
     }
 
     public double totalArea() {
         double total = 0;
         for (int i = 0; i < numberOfShapes; i++) {
-            total += shape[i].area();
+            total += shapes[i].area();
         }
         return total;
     }
@@ -41,7 +19,7 @@ public class ShapeList {
     public double totalPerimeter() {
         double total = 0;
         for (int i = 0; i < numberOfShapes; i++) {
-            total += shape[i].perimeter();
+            total += shapes[i].perimeter();
         }
         return total;
     }
--- solutions/hafizsheetab/IssueNo8/src/ShapeListTest.java
+++ solutions/hafizsheetab/IssueNo8/src/ShapeListTest.java
@@ -6,7 +6,15 @@ import org.junit.jupiter.api.Assertions;
 public class ShapeListTest {
     @Test
     public void testAll(){
-        ShapeList shapeList = new ShapeList(new double[]{10, 5, 1},new String[]{"square", "square", "circle"});
+        Shape[] shapes = new Shape[3];
+        shapes[0] = new Square();
+        shapes[0].setSize(10);
+        shapes[1] = new Square();
+        shapes[1].setSize(5);
+        shapes[2] = new Circle();
+        shapes[2].setSize(1);
+        ShapeList shapeList = new ShapeList(shapes);
+
         Assertions.assertEquals(66.2832, shapeList.totalPerimeter());
         Assertions.assertEquals(128.1416, shapeList.totalArea());
     }
